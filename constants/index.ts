62aa226427a2ac09a632906f3357d427b67a3f33
amresh012 <73485115+amresh012@users.noreply.github.com>
2024-07-04 10:20:45 +0530

    added sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarlink = [
+    {
+        label: "Home",
+        route: "/",
+        url:"/icons/Home.svg"
+    },
+    {
+        label: "Upcoming",
+        route: "/upcoming",
+        url: "/icons/upcoming.svg"
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        url: "/icons/previous.svg"
+    },
+    {
+        label: "Recording",
+        route: "/recordings",
+        url: "/icons/Video.svg"
+    },
+    {
+        label: "Personal Room",
+        route: "/personal-room",
+        url: "/icons/add-personal.svg"
+    },
+]
